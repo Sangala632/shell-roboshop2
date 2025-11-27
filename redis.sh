2f1f8985055752d57edfd47e8f4b46e60ee5538b
sangala632 <sangala2003@gmail.com>
2025-11-27 11:39:22 +0530

    changs sg id

--- redis.sh
+++ redis.sh
@@ -4,23 +4,22 @@ source ./common.sh
 App_name=redis
 check_root
 
-
-dnf module disable redis -y &>>$LOGFILE
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "disabling redis module"
 
-dnf module enable redis:7 -y &>>$LOGFILE
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "enabling redis 7 module"
 
-dnf install redis -y &>>$LOGFILE
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "installing redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis &>>$LOGFILE
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "enabling redis service"
 
-systemctl start redis &>>$LOGFILE
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "starting redis service"
 
 print_time
