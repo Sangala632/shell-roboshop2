d457b1a46e75a7cc1e4988d978eae1341f3ed887
sangala632 <sangala2003@gmail.com>
2025-11-27 11:53:12 +0530

    changs sg id

--- shipping.sh
+++ shipping.sh
@@ -11,21 +11,21 @@ app_setup
 maven_setup
 systemd_setup
 
-dnf install mysql -y  &>>$LOGFILE
+dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "installing the mysql"
 
 mysql -h mysql.hellodevsecops.space -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOGFILE
-    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOGFILE
-    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOGFILE
+    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h mysql.hellodevsecops.space  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "loading data into mysql"
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 fi
 
-systemctl restart shipping &>>$LOGFILE
+systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
 
 print_time
