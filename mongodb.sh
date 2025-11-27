eca47d5a3569b38e68ce0cc32b5d336b34597a7c
sangala632 <sangala2003@gmail.com>
2025-11-27 11:24:14 +0530

    changs sg id

--- mongodb.sh
+++ mongodb.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 source ./common.sh
-App_name=mongodb
+app_name=mongodb
 
 check_root
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "copying mongodb repo file"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "installing mongodb server"
+VALIDATE $? "Installing mongodb server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "enabling mongodb service"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "starting mongodb service"
+VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "allowing remote connections in mongodb config file"
+VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "restarting mongodb service"
+VALIDATE $? "Restarting MongoDB"
 
 print_time
-
-
