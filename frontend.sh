296886e830e3f980e325311f618a8ac7edd18524
sangala632 <sangala2003@gmail.com>
2025-11-27 11:55:07 +0530

    changs sg id

--- frontend.sh
+++ frontend.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 source ./common.sh
-App_name=frontend
-
 check_root
 
 dnf module disable nginx -y &>>$LOG_FILE
-VALIDATE $? "disabling nginx module"
+VALIDATE $? "Disabling Default Nginx"
 
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
-VALIDATE $? "enabling nginx 1.24 module"    
+VALIDATE $? "Enabling Nginx:1.24"
 
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "installing nginx"
+VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOG_FILE
+systemctl enable nginx  &>>$LOG_FILE
 systemctl start nginx 
-VALIDATE $? "starting nginx service"
+VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-VALIDATE $? "removing old nginx content"
+VALIDATE $? "Removing default content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading frontend content"
+VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "extracting frontend content"
+VALIDATE $? "unzipping frontend"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "copying nginx configuration file"
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove default nginx conf"
 
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "restarting nginx service"
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
-print_timecd
+systemctl restart nginx 
+VALIDATE $? "Restarting nginx"
+
+print_time
